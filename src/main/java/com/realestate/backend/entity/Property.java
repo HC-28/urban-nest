c8fc80ba16ec92e01c6f72851f24b63f50607787
Dev Vaghasiya <166021012+DevvVaghasiya@users.noreply.github.com>
2026-01-04 11:49:36 +0530

    Initial commit

---
+++ src/main/java/com/realestate/backend/entity/Property.java
@@ -0,0 +1,77 @@
+package com.realestate.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String type;
+    private double price;
+    private String photos;
+    private double area;
+    private int bhk;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public int getBhk() {
+        return bhk;
+    }
+
+    public void setBhk(int bhk) {
+        this.bhk = bhk;
+    }
+}
